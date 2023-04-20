9740d67dffcb7eb11b8af5a99a7897f06c15d785
Vittorio <vi.esposito@reply.it>
2023-04-20 17:58:30 +0200

    add class serializer interceptor

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,28 +1,31 @@
-import { Coffee } from "../../coffees/entities/coffee.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Coffee } from '../../coffees/entities/coffee.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 export enum Role {
-    User = 'user',
-    Admin = 'admin',
-  }
+  User = 'user',
+  Admin = 'admin',
+}
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ nullable: false, unique: true })
-    username: string;
+  @Column({ nullable: false, unique: true })
+  username: string;
 
-    @Column({ nullable: false })
-    password: string;
+  @Exclude()
+  @Column({ nullable: false })
+  password: string;
 
-    @Column({ nullable: false, enum: Role, default: Role.User })
-    role: Role;
+  @Column({ nullable: false, enum: Role, default: Role.User })
+  role: Role;
 
-    @OneToMany(
-        type => Coffee,
-        coffee => coffee.inventor
-    )
-    coffees: Coffee[]
+  @OneToMany((type) => Coffee, (coffee) => coffee.inventor)
+  coffees: Coffee[];
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
 }
